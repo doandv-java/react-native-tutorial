1b7e24d0131151cadb289213c84eaa9592c4b3e2
doandv <doandv188@gmail.com>
2022-11-17 16:11:30 +0700

    add api axios

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./src/components/Navigation";
+import {StatusBar} from 'react-native';
 import {AuthProvider} from "./src/context/AuthContent";
 
 const App = () => {
     return (
         <AuthProvider>
+            <StatusBar backgroundColor="#06bcee"/>
             <Navigation/>
         </AuthProvider>
     );
--- frontend/src/context/AuthContent.js
+++ frontend/src/context/AuthContent.js
@@ -1,6 +1,21 @@
 import React, {createContext} from "react";
+import axios from "axios";
+import {BASE_URL} from "../config";
 
 export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
+
+    const register = (name, email, password) => {
+        axios.post(`${BASE_URL}/register`, {
+            name, email, password
+        }).then(res => {
+            let userInfo = res.data;
+            console.log(`User Info: ${userInfo}`);
+            console.log(`Register success ${res}`);
+        }).catch(e => {
+            console.log(`Register error ${e}`);
+        });
+    }
+
     return (<AuthContext.Provider value="Test value">{children}</AuthContext.Provider>);
 };
