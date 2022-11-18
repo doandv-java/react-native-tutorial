abc01dc273e401d7625a95d47c25567291e22446
doandv <doandv@ominext.com>
2022-11-18 11:17:08 +0700

    update api for Articles

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('articles', [ArticleController::class,'index']);
+
+Route::get('articles/{article}',  [ArticleController::class,'show']);
+
+Route::post('articles',  [ArticleController::class,'store']);
+
+Route::put("articles/{article}",  [ArticleController::class,'update']);
+
+Route::delete('articles/{article}',  [ArticleController::class,'delete']);
